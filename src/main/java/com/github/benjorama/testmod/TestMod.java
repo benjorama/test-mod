1b4c4df87d05820c71cb03f5f78e07223c1e610c
benjorama <benmacomb@gmail.com>
2019-12-01 15:11:20 -0700

    Change value in @Mod annotation.

--- src/main/java/com/github/benjorama/testmod/TestMod.java
+++ src/main/java/com/github/benjorama/testmod/TestMod.java
@@ -2,7 +2,7 @@ package com.github.benjorama.testmod;
 
 import net.minecraftforge.fml.common.Mod;
 
-@Mod(value = "testmod")
+@Mod(TestMod.MODID)
 public class TestMod {
 	public static final String MODID = "testmod";
 }
