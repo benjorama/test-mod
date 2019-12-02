37cbc15eabf03b856262f20c7407119d9be120ef
benjorama <benmacomb@gmail.com>
2019-12-01 21:30:42 -0700

    Add logger to TestMod

--- src/main/java/com/github/benjorama/testmod/TestMod.java
+++ src/main/java/com/github/benjorama/testmod/TestMod.java
@@ -1,8 +1,15 @@
 package com.github.benjorama.testmod;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(TestMod.MODID)
 public class TestMod {
 	public static final String MODID = "testmod";
+	public static final Logger LOGGER = LogManager.getLogger(MODID);
+	
+	public TestMod() {
+		LOGGER.debug("Hello from testmod!");
+	}
 }
