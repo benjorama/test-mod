4e80aba7fcb7118dc74db2de8bfa977839bb7153
benjorama <benmacomb@gmail.com>
2019-12-02 19:30:08 -0700

    Implement an event subscriber to easily register new items.

---
+++ src/main/java/com/github/benjorama/testmod/ModEventSubscriber.java
@@ -0,0 +1,25 @@
+package com.github.benjorama.testmod;
+
+import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.registries.IForgeRegistryEntry;
+
+@EventBusSubscriber(modid = TestMod.MODID, bus = EventBusSubscriber.Bus.MOD)
+public class ModEventSubscriber {
+	
+	@SubscribeEvent
+	public static void onRegisterItems(RegistryEvent.Register<Item> event) {
+		event.getRegistry().registerAll(
+				setup(new Item(new Item.Properties()), "example_item"),
+				setup(new Item(new Item.Properties()), "example_item2"));
+	}
+	
+	public static <T extends IForgeRegistryEntry<T>> T setup(
+			final T entry, final String name) {
+		entry.setRegistryName(new ResourceLocation(TestMod.MODID, name));
+		return entry;
+	}
+}
