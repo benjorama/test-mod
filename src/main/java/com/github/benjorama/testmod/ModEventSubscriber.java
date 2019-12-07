a3999af09e9df412ffc05b8a56edefca7e065a9a
benjorama <benmacomb@gmail.com>
2019-12-06 18:59:06 -0700

    Add a new test block to the mod.

--- src/main/java/com/github/benjorama/testmod/ModEventSubscriber.java
+++ src/main/java/com/github/benjorama/testmod/ModEventSubscriber.java
@@ -2,6 +2,8 @@ package com.github.benjorama.testmod;
 
 import com.github.benjorama.testmod.init.ModItemGroups;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
 import net.minecraft.item.Item;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.RegistryEvent;
@@ -15,12 +17,19 @@ public class ModEventSubscriber {
 	@SubscribeEvent
 	public static void onRegisterItems(RegistryEvent.Register<Item> event) {
 		event.getRegistry().registerAll(
-				setup(new Item(
-						new Item.Properties()
-							.group(ModItemGroups.MOD_ITEM_GROUP))
+				setup(new Item(new Item.Properties().group(
+						ModItemGroups.MOD_ITEM_GROUP))
 						, "example_item"));
 	}
 	
+	@SubscribeEvent
+	public static void onRegisterBlocks(RegistryEvent.Register<Block> event) {
+		event.getRegistry().registerAll(
+				setup(new Block(Block.Properties.create(
+						Material.ROCK).hardnessAndResistance(3.0F, 3.0F))
+						, "example_block"));
+	}
+
 	public static <T extends IForgeRegistryEntry<T>> T setup(
 			final T entry, final String name) {
 		entry.setRegistryName(new ResourceLocation(TestMod.MODID, name));
